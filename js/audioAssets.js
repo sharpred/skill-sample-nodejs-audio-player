004737fe43ceb27d0a71ca013be48482671911f8
Paul Ryan <paul.ryan@stepupsoftware.co.uk>
2017-09-11 18:15:24 +0100

    mp3 links

--- js/audioAssets.js
+++ js/audioAssets.js
@@ -2,33 +2,28 @@
 
 // Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
 var audioData = [{
-        'title': 'Destination Moon',
+        'title': 'Dimension X, Requiem',
         'author': 'Robert Heinlein',
-        'url': 'https://soundcloud.com/user-496255760/destination-moon-1'
+        'url': 'http://feeds.soundcloud.com/stream/341898539-user-496255760-requiem.mp3'
     },
     {
         'title': 'The Tunnel Under The World',
         'author': 'Frederick Pohl',
-        'url': 'https://soundcloud.com/user-496255760/tunnel-under-the-world-1'
+        'url': 'http://feeds.soundcloud.com/stream/341900620-user-496255760-frederick-pohl-tunnel-under-the-world.mp3'
     },
     {
         'title': 'Nightfall',
         'author': 'Isaac Asimov',
-        'url': 'https://soundcloud.com/user-496255760/nightfall-1'
+        'url': 'http://feeds.soundcloud.com/stream/341901140-user-496255760-nightfall.mp3'
     },
     {
-        'title': 'The Defenders',
+        'title': 'X Minus One, The Defenders',
         'author': 'Philip K Dick',
-        'url': 'https://soundcloud.com/user-496255760/x-minus-one'
-    },
-    {
-        'title': 'The Time Machine',
-        'author': 'H G Wells',
-        'url': 'https://soundcloud.com/user-496255760/the-time-machine-hg-wells'
+        'url': 'http://feeds.soundcloud.com/stream/341900490-user-496255760-x-minus-one-the-defenders.mp3'
     }, {
         'title': 'The Martian Chronicles',
         'author': 'Ray Bradbury',
-        'url': 'https://soundcloud.com/user-496255760/the-martian-chronicles-1'
+        'url': 'http://feeds.soundcloud.com/stream/341899932-user-496255760-the-martian-chronicles.mp3'
     }
 ];
 
