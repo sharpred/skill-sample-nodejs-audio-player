4f57801f1628732cdd4ec8c9eeb6e3c85e8e1e55
Paul Ryan <paul.ryan@stepupsoftware.co.uk>
2017-09-11 12:45:13 +0100

    use scifi london assets

--- js/audioAssets.js
+++ js/audioAssets.js
@@ -1,14 +1,34 @@
 'use strict';
 
 // Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
-var audioData = [
-    {
-        'title' : 'Episode 140',
-        'url' : 'https://feeds.soundcloud.com/stream/275202399-amazon-web-services-306355661-amazon-web-services.mp3'
+var audioData = [{
+        'title': 'Destination Moon',
+        'author': 'Robert Heinlein',
+        'url': 'https://soundcloud.com/user-496255760/destination-moon-1'
     },
     {
-        'title' : 'Episode 139',
-        'url' : 'https://feeds.soundcloud.com/stream/274166909-amazon-web-services-306355661-aws-podcast-episode-139.mp3'
+        'title': 'The Tunnel Under The World',
+        'author': 'Frederick Pohl',
+        'url': 'https://soundcloud.com/user-496255760/tunnel-under-the-world-1'
+    },
+    {
+        'title': 'Nightfall',
+        'author': 'Isaac Asimov',
+        'url': 'https://soundcloud.com/user-496255760/nightfall-1'
+    },
+    {
+        'title': 'The Defenders',
+        'author': 'Philip K Dick',
+        'url': 'https://soundcloud.com/user-496255760/x-minus-one'
+    },
+    {
+        'title': 'The Time Machine',
+        'author': 'H G Wells',
+        'url': 'https://soundcloud.com/user-496255760/the-time-machine-hg-wells'
+    }, {
+        'title': 'The Martian Chronicles',
+        'author': 'Ray Bradbury',
+        'url': 'https://soundcloud.com/user-496255760/the-martian-chronicles-1'
     }
 ];
 
